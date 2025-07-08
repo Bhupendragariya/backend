ba43d1e95480c9e61afd3261b0335aa439321741
bhupendra gariya <bhupiigarya7@gmail.com>
2025-07-08 15:40:16 +0530

    create a backend

---
+++ app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
+import cors from "cors";
+
+
+
+dotenv.config()
+
+
+
+const app = express()
+
+
+
+
+app.use(cors());
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send('Hello from backend!');
+});
+
+
+
+
+
+
+const PORT = process.env.PORT || 4000
+
+
+app.listen(PORT, () =>{
+    console.log(`Server is listening on http://localhost:${PORT}`);
+})
